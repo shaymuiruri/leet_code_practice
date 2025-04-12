0f67239f8926627cc68b41f9fbd272a379df2037
Mitchelle Muiruri <121823073+shaymuiruri@users.noreply.github.com>
2025-04-12 12:02:14 +0300

    Add files via upload
    1-100 , 11 22 33 44 55 66 77 88 99

---
+++ count_symmetric_intergers.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countSymmetricIntegers(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        count = 0
+        for num in range(low, high + 1):
+            s = str(num)
+            if len(s) % 2 == 0:
+                n = len(s) // 2
+                left_sum = sum(int(d) for d in s[:n])
+                right_sum = sum(int(d) for d in s[n:])
+                if left_sum == right_sum:
+                    count += 1
+        return count
+
+        
